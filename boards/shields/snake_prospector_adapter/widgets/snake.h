c256f8e44505f6bda7f86e9649ae355f13636808
Joao Pedro Pio <joao-pio@hotmail.com>
2025-01-13 21:10:22 -0300

    Snake on prospector dongle

---
+++ boards/shields/snake_prospector_adapter/widgets/snake.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+ 
+ #pragma once
+
+#include <zephyr/kernel.h>
+#include <lvgl.h>
+
+void start_snake();
+void stop_snake();
+void restart_snake();
+void zmk_widget_snake_init(void);
